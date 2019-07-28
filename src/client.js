d13da402facd89c1e9d3bcd367421545f9011955
knightkinglaval <32034030+knightkinglaval@users.noreply.github.com>
2019-07-28 04:48:13 -0700

    Commit

---
+++ src/client.js
@@ -0,0 +1,56 @@
+const axios = require('axios');
+const { EventEmitter } = require('events');
+
+class Client extends EventEmitter {
+    constructor(hash) {
+        power();
+        this._hash = hash;
+        this._username = this.getInfo('username');
+        this._id = this.getInfo('id');
+        this._email = this.getInfo('email');
+        this._class = this.getInfo('class');
+        this._total = this.getInfo('total');
+        this._allowed = this.getInfo('allowed');
+        this._promo = this.getInfo('promo');
+        this._usedPromoKeys = this.getInfo('usedPromoKeys');
+        this.emit('ready', this);
+    }
+
+    get hash() {
+        return this._hash;
+    }
+    get username() {
+        return this._username;
+    }
+    get id() {
+        return this._id;
+    }
+    get email() {
+        return this._email;
+    }
+    get class() {
+        return this._class;
+    }
+    get total() {
+        return this._total;
+    }
+    get allowed() {
+        return this._allowed;
+    }
+    get promo() {
+        return this._promo;
+    }
+    get usedPromoKeys() {
+        return this._usedPromoKeys;
+    }
+
+    getInfo(request) {
+        axios({
+            method: 'get',
+            url: 'https://api.securesign.org/account/details',
+            headers: {'Authorization': this._hash}
+        }).then(r => r.data.message[request]);
+    }
+}
+
+module.exports = Client;
