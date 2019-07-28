60c0a760f91ead552648b7170d94225533e4cd7e
knightkinglaval <32034030+knightkinglaval@users.noreply.github.com>
2019-07-28 14:23:43 -0700

    Commit

--- src/client.js
+++ src/client.js
@@ -57,6 +57,45 @@ class Client extends EventEmitter {
 
        return method.data.message[request];
     }
+
+    async generateKey(type, info) {
+        if(!type) throw new Error('Must specify whether creating an ECC or RSA key.');
+        if(!info) throw new Error('Must provide either a curve for an ECC key, or a modulus number for an RSA key.');
+        if(type !== 'ECC' || 'RSA') throw new Error('Type of key must either be ECC or RSA.');
+        if(type === 'ECC') {
+            axios({
+                method: 'post',
+                url: 'https://api.securesign.org/keys/ecc',
+                headers: {'authorization': this._hash, 'Content-Type': 'application/json'},
+                data: JSON.stringify({curve: curve})
+            }).then(r => r.data);
+        } else if (type === 'RSA') {
+            axios({
+                method: 'post',
+                url: 'https://api.securesign.org/keys/rsa',
+                headers: {'authorization': this._hash, 'Content-type': 'application/json'},
+                data: JSON.stringify({modulus: type})
+            }).then(r => r.data);
+        }
+    }
+
+    async listCurves() {
+        axios({
+            method: 'get',
+            url: 'https://api.securesign.org/keys/ecc',
+            headers: {'authorization': this._hash}
+        }).then(r => r.data);
+    }
+
+    async listCerts() {
+        axios({
+            method: 'get',
+            url: 'https://api.securesign.org/certificates',
+            headers: {'authorization': this._hash}
+        }).then(r => r.data);
+    }
+
+    
 }
 
 module.exports = Client;
